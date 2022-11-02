bdafaa59de74eef21bd35e0b87a0d2e5e326e865
Wilson Félix <wilson.felix.jr@gmail.com>
2022-11-02 10:30:45 -0300

    Camada Presentation e camada Infra.Data 2

---
+++ AgendaWeb.Infra.Data/Entities/EventoEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaWeb.Infra.Data.Entities
+{
+    /// <summary>
+    /// Classe de entidade para EventoEntity
+    /// </summary>
+    public class EventoEntity
+    {
+        #region
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime? Data { get; set; }
+        public TimeSpan? Hora { get; set; }
+        public string? Descricao { get; set; }
+        public int? Prioridade { get; set; }
+        public DateTime? DataInclusao { get; set; }
+        public DateTime? DataAlteracao { get; set; }
+        public int? Ativo { get; set; }
+        #endregion
+    }
+}
