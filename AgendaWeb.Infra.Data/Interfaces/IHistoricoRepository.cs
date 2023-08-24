8b544e9d994088c15d5b7eb91465eece000c7529
wilson.junior <wilson.junior@saude.rj.gov.br>
2023-08-24 10:16:09 -0300

    Atualização do Projeto

--- AgendaWeb.Infra.Data/Interfaces/IHistoricoRepository.cs
+++ AgendaWeb.Infra.Data/Interfaces/IHistoricoRepository.cs
@@ -12,17 +12,18 @@ namespace AgendaWeb.Infra.Data.Interfaces
     /// </summary>
     public interface IHistoricoRepository : IBaseRepository<Historico>
     {
+        new
 
-        /// <summary>
-        /// Método para retornar todos os eventos dentro de um período de data na tabela de HISTORICO
-        /// </summary>
-        /// <param name="DataMin">Data de início do período</param>
-        /// <param name="DataMax">Data de término do período</param>
-        /// <param name="Ativo">Flag 0 para inativo ou 1 para ativo</param>
-        /// <returns>Lista de eventos</returns>
-        //List<Evento> GetByDatas(DateTime? DataMin, DateTime? DataMax, int? Ativo);
-        //List<Evento> GetAll(DateTime? DataMin, DateTime? DataMax, int? Ativo);
-        Historico? GetById(Guid Id);
-        List<Historico> GetAll(DateTime? DataMin, DateTime? DataMax);
+                /// <summary>
+                /// Método para retornar todos os eventos dentro de um período de data na tabela de HISTORICO
+                /// </summary>
+                /// <param name="DataMin">Data de início do período</param>
+                /// <param name="DataMax">Data de término do período</param>
+                /// <param name="Ativo">Flag 0 para inativo ou 1 para ativo</param>
+                /// <returns>Lista de eventos</returns>
+                //List<Evento> GetByDatas(DateTime? DataMin, DateTime? DataMax, int? Ativo);
+                //List<Evento> GetAll(DateTime? DataMin, DateTime? DataMax, int? Ativo);
+                Historico? GetById(Guid Id);
+                new List<Historico> GetAll(DateTime? DataMin, DateTime? DataMax);
     }
 }
